84e36777c3ceb660821a19805bf1c48a64c28348
Anton Murygin <mibus32@gmail.com>
2017-08-18 21:53:25 +0300

    Add refunded by user id

--- app/models/concerns/refundable.rb
+++ app/models/concerns/refundable.rb
@@ -3,11 +3,11 @@ module Refundable
   included do
     has_many :refunds
 
-    def make_refund(amount:, reason:, test:nil)
+    def make_refund(amount:, reason:, refunded_by_user_id:, test:nil)
       activate_session
       credit = ShopifyAPI::ApplicationCredit.new(amount: amount, description: reason, test: test)
       if credit.save
-        refund = self.refunds.create reason: reason, amount_usd: amount
+        refund = self.refunds.create reason: reason, amount_usd: amount, refunded_by_user_id: refunded_by_user_id
       else
         if credit.valid?
           error = "Failed.  Response code = 422.  Response message = Unprocessable Entity"
@@ -15,7 +15,7 @@ module Refundable
           error = credit.errors.full_messages.join(' ')
         end
 
-        refund = self.refunds.create reason: reason, amount_usd: amount, errored: true, error_message: error
+        refund = self.refunds.create reason: reason, amount_usd: amount, errored: true, error_message: error, refunded_by_user_id: refunded_by_user_id
       end
 
       return refund

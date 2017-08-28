f603ffedb5a9f9e028f4da6c510f99d1dbd2933b
Anton Murygin <mibus32@gmail.com>
2017-08-28 15:39:20 +0300

    Rescue from exceptions

--- app/models/concerns/refundable.rb
+++ app/models/concerns/refundable.rb
@@ -6,18 +6,19 @@ module Refundable
     def make_refund(amount:, reason:, refunded_by_user_id:, test:nil)
       activate_session
       credit = ShopifyAPI::ApplicationCredit.new(amount: amount, description: reason, test: test)
-      if credit.save
+      if credit.save!
         refund = self.refunds.create reason: reason, amount_usd: amount, refunded_by_user_id: refunded_by_user_id
-      else
-        if credit.valid?
-          error = "Failed.  Response code = 422.  Response message = Unprocessable Entity"
-        else
-          error = credit.errors.full_messages.join(' ')
-        end
-
-        refund = self.refunds.create reason: reason, amount_usd: amount, errored: true, error_message: error, refunded_by_user_id: refunded_by_user_id
+        return refund
       end
 
+    rescue ActiveResource::ClientError, ActiveResource::ResourceInvalid => e
+      if credit.valid?
+        error = e.message
+      else
+        error = credit.errors.full_messages.join(' ')
+      end
+
+      refund = self.refunds.create reason: reason, amount_usd: amount, errored: true, error_message: error, refunded_by_user_id: refunded_by_user_id
       return refund
     end
   end

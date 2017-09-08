b4d3732c60e47db3a576a87d62856054de1623e8
Anton Murygin <mibus32@gmail.com>
2017-09-08 19:38:12 +0300

    Use column name from config

--- lib/pr/common/configuration.rb
+++ lib/pr/common/configuration.rb
@@ -1,7 +1,7 @@
 module PR
   module Common
     class Configuration
-      attr_accessor :signup_params, :send_welcome_email, :send_confirmation_email
+      attr_accessor :signup_params, :send_welcome_email, :send_confirmation_email, :premium_column_name
     end
   end
 end

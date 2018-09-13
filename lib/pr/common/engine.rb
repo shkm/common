2ce8a50eb7c2f8752c10dcdac25bd7e62f4219ae
Daniel Sim <me@danielsim.co.uk>
2018-09-13 16:03:22 +0200

    Enable migrations to live in common

--- lib/pr/common/engine.rb
+++ lib/pr/common/engine.rb
@@ -1,10 +1,16 @@
 module PR
   module Common
     class Engine < ::Rails::Engine
-
       config.generators do |g|
         g.test_framework :rspec
         g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      end
+      initializer :append_migrations do |app|
+        unless app.root.to_s.match root.to_s
+          config.paths["db/migrate"].expanded.each do |expanded_path|
+            app.config.paths["db/migrate"] << expanded_path
+          end
+        end
       end
     end
   end

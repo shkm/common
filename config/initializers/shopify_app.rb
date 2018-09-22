81649dd619f0f489b913d4526483d859e49748e3
Daniel Sim <me@danielsim.co.uk>
2018-09-22 17:04:10 -0400

    Remove ngrok URL

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,9 +1,10 @@
 ShopifyApp.configure do |config|
   # For non-common, application specific webhooks be sure to do config.webhooks.push() rather than overwriting those here
   # TODO: Check and document what happens when adding/removing webhooks from here for existing shops?
+  # https://pluginseo.ngrok.io
   config.webhooks = [
-      { topic: 'app/uninstalled', address: "https://pluginseo.ngrok.io/webhooks/app_uninstalled", format: 'json' },
-      { topic: 'shop/update', address: "https://pluginseo.ngrok.io/webhooks/shop_update", format: 'json' },
+      { topic: 'app/uninstalled', address: "#{Settings.api_url}/webhooks/app_uninstalled", format: 'json' },
+      { topic: 'shop/update', address: "#{Settings.api_url}/webhooks/shop_update", format: 'json' },
   ]
   # This would be nice to organise the jobs:
   # config.webhook_jobs_namespace = 'shopify/webhooks'

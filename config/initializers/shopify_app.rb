01128f9b1a82052d753630ed6fe9581a7896dc40
Daniel Sim <me@danielsim.co.uk>
2018-09-25 16:07:39 +0200

    Change to webhook_url to support local dev with ngrok

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,8 +3,8 @@ ShopifyApp.configure do |config|
   # TODO: Check and document what happens when adding/removing webhooks from here for existing shops?
   # https://pluginseo.ngrok.io
   config.webhooks = [
-      { topic: 'app/uninstalled', address: "#{Settings.api_url}/webhooks/app_uninstalled", format: 'json' },
-      { topic: 'shop/update', address: "#{Settings.api_url}/webhooks/shop_update", format: 'json' },
+      { topic: 'app/uninstalled', address: "#{Settings.webhook_url}/webhooks/app_uninstalled", format: 'json' },
+      { topic: 'shop/update', address: "#{Settings.webhook_url}/webhooks/shop_update", format: 'json' },
   ]
   # This would be nice to organise the jobs:
   # config.webhook_jobs_namespace = 'shopify/webhooks'

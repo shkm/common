d3475657e3635a4d562e9e73704fd3fbd0f5f457
Anton Murygin <mibus32@gmail.com>
2018-10-08 16:33:43 +0300

    Add Shop methods

---
+++ lib/pr/common/models/shop.rb
@@ -0,0 +1,18 @@
+module PR
+  module Common
+    module Models
+      module Shop
+        extend ActiveSupport::Concern
+        included do
+          scope :with_active_plan, -> { where.not(shops: { plan_name: 'cancelled' }) }
+          scope :with_active_charge, -> { joins('JOIN users ON shops.id = users.shop_id').where(users: { active_charge: true }) }
+          scope :installed, -> { where(uninstalled: false) }
+        end
+
+        class_methods do
+          # add class methods here
+        end
+      end
+    end
+  end
+end

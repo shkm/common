75f2d49276f959ca20a7b6cd6c72f6e0a6f49ef8
Anton Murygin <mibus32@gmail.com>
2018-10-08 16:33:48 +0300

    Fix User model

--- lib/pr/common/models/user.rb
+++ lib/pr/common/models/user.rb
@@ -6,9 +6,15 @@ module PR
       # We will work towards this by including this module in our apps and moving generic pieces here
       # So please include PR::Common::Models::User
       module User
-        def self.included(base)
-          alias_method :has_active_charge?, :active_charge
-          alias_method :active_charge?, :active_charge
+        extend ActiveSupport::Concern
+        included do
+          [:has_active_charge?, :active_charge?].each do |name|
+            send(:define_method, name, -> { self.active_charge })
+          end
+        end
+
+        class_methods do
+          # add class methods here
         end
       end
     end

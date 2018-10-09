b3c298988d89a006d480e4f74d68314b7a7ef4f7
Jamie Schembri <jamie@schembri.me>
2018-10-09 10:10:09 +0200

    Update migrations from 5.2 to 5.0

--- db/migrate/20181008114742_make_plan_name_not_nullable.rb
+++ db/migrate/20181008114742_make_plan_name_not_nullable.rb
@@ -1,4 +1,4 @@
-class MakePlanNameNotNullable < ActiveRecord::Migration[5.2]
+class MakePlanNameNotNullable < ActiveRecord::Migration[5.0]
   def change
     change_column_null :shops, :plan_name, false
   end

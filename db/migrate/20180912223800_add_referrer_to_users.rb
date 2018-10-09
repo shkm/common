6273dc45fd1ea5af50ba2578c9be289a6f847966
Jamie Schembri <jamie@schembri.me>
2018-10-09 11:26:38 +0200

    Make migrations more robust with checks to prevent failure

--- db/migrate/20180912223800_add_referrer_to_users.rb
+++ db/migrate/20180912223800_add_referrer_to_users.rb
@@ -1,5 +1,9 @@
 class AddReferrerToUsers < ActiveRecord::Migration[5.0]
   def change
+    # prevent this migration failing for apps that already have a
+    # referrer on users
+    return if column_exists? :users, :referrer
+
     add_column :users, :referrer, :string
   end
 end
--- db/migrate/20181008151650_add_provider_to_users.rb
+++ db/migrate/20181008151650_add_provider_to_users.rb
@@ -1,5 +1,7 @@
 class AddProviderToUsers < ActiveRecord::Migration[5.0]
   def change
+    # prevent this migration failing for apps that already have a
+    # provider on users
     return if column_exists? :users, :provider
 
     add_column :users, :provider, :integer
